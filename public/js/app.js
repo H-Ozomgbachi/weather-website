de689e8013b26549fca568e1384887907f2d39b6
H-ozomgbachi <ozomgbachih@gmail.com>
2021-05-22 09:12:32 +0100

    Remove unnecessary console messge

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+
 const messageOne = document.getElementById("message-1");
 const messageTwo = document.getElementById("message-2");
 
